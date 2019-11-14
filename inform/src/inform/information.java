10b6c7e6232ddc69ebbf36fe05808e2ddc146c75
Yeri Cho <yery0501@gmail.com>
2019-11-14 09:22:50 +0900

    master branch charge a code

--- inform/src/inform/information.java
+++ inform/src/inform/information.java
@@ -5,6 +5,7 @@ public class information {
 	public static void main(String[] args) {
 		System.out.println("Computer SW");
 		System.out.println("Wonkwang Univ.");
+		System.out.println("Á¶¿¹¸®");
 	}
 
 }
